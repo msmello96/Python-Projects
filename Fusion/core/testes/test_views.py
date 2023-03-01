1a9e3a1d151aa9372d4d27885fb6f56fba3e9d63
msmello96 <mello.marcelo1996@gmail.com>
2023-03-01 11:06:58 -0300

    Atualização de portifólio

---
+++ Fusion/core/testes/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.dados = {
+            'nome': 'Felicity Jones',
+            'email': 'felicity@gmail.com',
+            'assunto': 'Um assunto qualquer',
+            'mensagem': 'Uma mensagem qualquer'
+        }
+        self.cliente = Client()
+
+    def test_form_valid(self):
+        request = self.client.post(reverse_lazy('index'), data=self.dados)
+        self.assertEquals(request.status_code, 302)
+
+    def test_form_invalid(self):
+        dados = {
+            'nome': 'Felicity Jones',
+            'assunto': 'Qualquer Assunto'
+        }
+        request = self.cliente.post(reverse_lazy('index'), data=dados)
+        self.assertEquals(request.status_code, 200)
+
